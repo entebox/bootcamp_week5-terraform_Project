2d037a1e74bd4cdd9b6e4fb59c28bf6fac5c6198
entebox <67682708+entebox@users.noreply.github.com>
2021-10-19 02:43:15 +0300

    Update postgressvc.tf

--- terraform/postgressvc.tf
+++ terraform/postgressvc.tf
@@ -1,12 +1,7 @@
-resource "azurerm_resource_group" "rg_b" {
-  name     = var.resource_group_bonus_b
-  location = "westus2"
-}
-
 resource "azurerm_postgresql_server" "postgsvc" {
   name                = var.postgressvc_name
   location            = "westus2"
-  resource_group_name = var.resource_group_bonus_b
+  resource_group_name = var.resource_group_name
 
   sku_name = var.postgressvc_sku_name
 
@@ -19,22 +14,30 @@ resource "azurerm_postgresql_server" "postgsvc" {
   administrator_login_password = var.postgresvc_password
   version                      = var.postgressvc_version
   ssl_enforcement_enabled      = false
+  depends_on = [ azurerm_resource_group.rg ]
 }
 
 resource "azurerm_postgresql_database" "db" {
   name                = "app_db"
-  resource_group_name = var.resource_group_bonus_b
+  resource_group_name = var.resource_group_name
   server_name         = var.postgressvc_name
   charset             = "UTF8"
   collation           = "English_United States.1252"
+  depends_on = [ azurerm_resource_group.rg, azurerm_postgresql_server.postgsvc ]
 }
 
 resource "azurerm_postgresql_firewall_rule" "postgres_firewall" {
-  name                = var.ip_public_name[count.index]
-  resource_group_name = var.resource_group_bonus_b
+  name                = "webSrv_public_ip${count.index + 1}"
+  resource_group_name = var.resource_group_name
   server_name         = var.postgressvc_name
-  start_ip_address    = azurerm_public_ip.webpublicip[count.index]
-  end_ip_address      = azurerm_public_ip.webpublicip[count.index]
-  count               = length(var.ip_public_name)
+  start_ip_address    = data.azurerm_public_ip.pubip[count.index].ip_address
+  end_ip_address      = data.azurerm_public_ip.pubip[count.index].ip_address
+  count               = var.websrvs_quantity
   depends_on          = [ azurerm_public_ip.webpublicip ]
+}
+
+data "azurerm_public_ip" "pubip" {
+  name                = azurerm_public_ip.webpublicip[count.index].name
+  resource_group_name = var.resource_group_name
+  count               = var.websrvs_quantity
 }
