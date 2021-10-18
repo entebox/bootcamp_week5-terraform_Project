a434633896bd796f08d5b89edee39b8e22e5568e
entebox <67682708+entebox@users.noreply.github.com>
2021-10-19 02:36:29 +0300

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,8 @@
+#random password
 output "vm_password_servers" {
 value = nonsensitive(random_password.password.result)
+}
+#output public ip of web servers
+output "srv_public_ips" {
+  value       = data.azurerm_public_ip.pubip.*.ip_address
 }
