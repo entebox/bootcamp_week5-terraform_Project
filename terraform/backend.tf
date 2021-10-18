4f6893ad065e89087eb0ed67c824f8dbf89eb561
entebox <67682708+entebox@users.noreply.github.com>
2021-10-18 21:30:29 +0300

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,30 +1,15 @@
-#--------------------Create Blob Storage for terraform state---------------#
-resource "random_string" "resource_code" {
-  length  = 5
-  special = false
-  upper   = false
-}
-
-resource "azurerm_resource_group" "tfstate" {
-  name     = "tfstate"
-  location = var.location
-}
-
-resource "azurerm_storage_account" "tfstate" {
-  name                     = "tfstate${random_string.resource_code.result}"
-  resource_group_name      = azurerm_resource_group.tfstate.name
-  location                 = azurerm_resource_group.tfstate.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  allow_blob_public_access = true
-
-  tags = {
-    environment = "staging"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
   }
-}
+    backend "azurerm" {
+        resource_group_name  = "tfstate"
+        storage_account_name = "tfstatemjlzz"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
 
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.tfstate.name
-  container_access_type = "blob"
 }
