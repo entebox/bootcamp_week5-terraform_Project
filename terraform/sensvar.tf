1454852fc44b3f67c0653f66f12b6b4e0e3ca368
entebox <67682708+entebox@users.noreply.github.com>
2021-10-18 20:55:04 +0300

    Update sensvar.tf
    removed sensetive data

--- terraform/sensvar.tf
+++ terraform/sensvar.tf
@@ -1,15 +1,12 @@
 variable "admin_username" {
   description = "vm user name"
   type        = string
-  default = "aviberger"
   sensitive = true
 }
 variable "admin_password" {
   description = "vm password"
   type        = string
-  default = "Passw0rd1234!"
 }
 variable "nsg_source_ip" {
-  default = "213.137.80.32/32"
   sensitive = true
 }
